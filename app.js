d4c8c84133e31dfef2edb531326b7dabe328186d
das <tree@gmail.com>
2025-06-18 18:35:58 +0200

    2025/06/18

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 // npm i express ijs
+
+const path = require("node:path")
+const express = require("express")
+const app = express()
+
+process.loadEnvFile();
+const PORT = process.env.PORT || 8888;
+
+
+//middleware, entre  request y  response
+app.use(express.static(path.join(__dirname, "public"))) // para que el programa vea los recursos en public, sino solo ve 404, lo demas es 404
+
+
+app.get("/",(req,res)=>{
+    res.send("todo ok")
+})
+
+
+
+app.use((req,res)=>{ /* else final, ruta desconocida*/
+    res.status(404).sendFile(path.join(__dirname, "public", "404.html"))
+}) 
+
+app.listen(PORT, ()=>{
+    console.log(`servidor iniciado en http://localhost:${PORT}`);
+})
